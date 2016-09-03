c295e5ccd8ae7be01403782f9ebde3b121ac8eb6
Stavros Polymenis <sp@orbitalfox.com>
2016-09-03 13:14:29 +0100

    introducion of tyxml

---
+++ src/html.ml
@@ -0,0 +1,9 @@
+let html_of ymd =
+  let ymd_title = ymd.Logarion.meta.title in
+  let ymd_body = Omd.to_html (Omd.of_string ymd.Logarion.text) in
+  let open Tyxml.Html in
+  let tyhtml = html
+    (head (title (Unsafe.data ymd_title)) [])
+    (body [Unsafe.data ymd_body])
+    in
+  Format.asprintf "%a" (Tyxml.Html.pp ()) tyhtml
