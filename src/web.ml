7a4b7c91059ed3f817ca2ec2e2eacf28934f1d4b
Stavros Polymenis <sp@orbitalfox.com>
2016-09-05 21:38:11 +0100

    simple css support

--- src/web.ml
+++ src/web.ml
@@ -1,11 +1,5 @@
 open Opium.Std
 
-let test = "---
-title: Test
-author: orbifx
----
-Hello _world_!"
-
 let load_file f =
   let ic = open_in f in
   let n = in_channel_length ic in
@@ -13,14 +7,18 @@ let load_file f =
   really_input ic s 0 n;
   close_in ic;
   (s)
-             
+
+let print_css =
+  get "/style.css" begin fun req -> `String (load_file "ymd/style.css") |> respond' end
+    
 let print_ymd =
   get "/:title" begin fun req ->
-                let file = (param req "title") ^ ".ymd" in
+                let file = "ymd/" ^ (param req "title") ^ ".ymd" in
                 `Html (Html.html_of (Logarion.ymd (load_file file))) |> respond'
                 end
-                       
+
 let _ =
   App.empty
   |> print_ymd
+  |> print_css
   |> App.run_command
