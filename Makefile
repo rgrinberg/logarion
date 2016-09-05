0bb80fecaf076bae481c5d4041e4478f92d4dc15
Stavros Polymenis <sp@orbitalfox.com>
2016-09-05 20:35:43 +0100

    simple serving of ymd files

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 
 all: web
 
-web: logarion.cmx src/web.ml
-	ocamlfind ocamlopt -o logarion-web -linkpkg -package opium.unix,omd,Str logarion.cmx src/web.ml
+web: logarion.cmx html.cmx src/web.ml
+	ocamlfind ocamlopt -o logarion-web -linkpkg \
+	-package opium.unix,omd,Str,tyxml \
+	logarion.cmx html.cmx src/web.ml
+
+html.cmx: src/html.ml logarion.cmx
+	ocamlfind ocamlopt -c -o html.cmx -linkpkg \
+	-package omd,tyxml \
+	logarion.cmx src/html.ml
 
 logarion.cmx: src/logarion.ml
 	ocamlfind ocamlopt -c -o logarion.cmx -linkpkg -package omd src/logarion.ml
--- src/web.ml
+++ src/web.ml
@@ -5,12 +5,20 @@ title: Test
 author: orbifx
 ---
 Hello _world_!"
+
+let load_file f =
+  let ic = open_in f in
+  let n = in_channel_length ic in
+  let s = String.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  (s)
              
 let print_ymd =
-  let open Logarion in
-  get "/" begin fun req ->
-          `Html (html_of (ymd test)) |> respond'
-          end
+  get "/:title" begin fun req ->
+                let file = (param req "title") ^ ".ymd" in
+                `Html (Html.html_of (Logarion.ymd (load_file file))) |> respond'
+                end
                        
 let _ =
   App.empty
