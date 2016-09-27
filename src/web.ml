09821b919862a763a3f55ad5893d242a04821f5f
Stavros Polymenis <sp@orbitalfox.com>
2016-09-28 00:26:54 +0100

    refactoring

--- src/web.ml
+++ src/web.ml
@@ -1,28 +1,8 @@
 open Opium.Std
 
-let sanitised_path path =
-  let parent = Str.regexp "../" in
-  Str.global_replace parent "" path
-
-let print_css =
-  get "/style.css"
-      begin
-        fun req -> `String (Logarion.load_file "ymd/style.css") |> respond'
-      end
-
-let print_ymd =
-  get "/:title"
-      begin fun req ->
-      let filename = sanitised_path (param req "title") in
-      let filepath = "ymd/" ^ filename ^ ".ymd" in
-      `Html (Html.of_ymd (Logarion.of_file filepath)) |> respond'
-      end
-
-let print_form =
-  get "/()/new"
-      begin fun req ->
-      `Html (Html.form (Ymd.blank_ymd)) |> respond'
-      end
+let filepath_of_param req prm =
+  let filename = Ymd.filename_of_title @@ param req prm in
+  "ymd/" ^ filename
 
 let ymd_of_body_pairs pairs =
   let open Ymd in
@@ -30,26 +10,21 @@ let ymd_of_body_pairs pairs =
   ListLabels.fold_left ~f:(fun a (k,vl) -> with_kv a (k, List.hd vl) ) ~init:blank_ymd pairs
   |> ((ymd_meta |-- meta_date |-- date_edited) ^= Some (Ptime_clock.now ()))
 
-let process_form =
-  post "/()/new"
-       begin fun req ->
-       let pairs = Lwt_main.run @@ App.urlencoded_pairs_of_body req in
-       let open Logarion in
-       let ymd = ymd_of_body_pairs pairs in
-       to_file ymd;
-       `Html (Html.of_ymd ymd) |> respond'
-       end
+let ymd_of_req req =
+  let pairs = Lwt_main.run @@ App.urlencoded_pairs_of_body req in
+  ymd_of_body_pairs pairs
 
-let edit_form =
-  get "/:title/edit"
-      begin fun req ->
-      let filename = sanitised_path (param req "title") in
-      let filepath = "ymd/" ^ filename ^ ".ymd" in
-      `Html (Html.form (Logarion.of_file filepath)) |> respond'
-      end
+let file_response file = `String (Logarion.load_file file) |> respond'
+let ymd_response ymd = `Html (Html.of_ymd (Logarion.of_file ymd)) |> respond'
+let form_response ymd = `Html (Html.form (Logarion.of_file ymd)) |> respond'
 
-let print_toc =
-  get "/" begin fun req -> `Html (Html.of_titled_files (Logarion.titled_files ())) |> respond' end
+let print_css  = get "/style.css" @@ (fun _ -> file_response "ymd/style.css")
+let print_ymd  = get "/:title" @@ (fun req -> filepath_of_param req "title" |> ymd_response)
+let print_form = get "/()/new" @@ (fun _ -> `Html (Html.form (Ymd.blank_ymd)) |> respond')
+
+let process_form = post "/()/new" (fun req -> let ymd = ymd_of_req req in Logarion.to_file ymd; `Html (Html.of_ymd ymd) |> respond')
+let edit_form = get "/:title/edit" (fun req -> filepath_of_param req "title" |> form_response)
+let print_toc = get "/" (fun req -> `Html (Html.of_titled_files (Logarion.titled_files ())) |> respond')
 
 let _ =
   App.empty
