05f1bb10ba99570f72a54bfc7f073d16c33ca858
Stavros Polymenis <sp@orbitalfox.com>
2016-09-29 00:04:07 +0100

    refactoring

--- src/web.ml
+++ src/web.ml
@@ -18,20 +18,12 @@ let file_response file = `String (Logarion.load_file file) |> respond'
 let ymd_response ymd = `Html (Html.of_ymd (Logarion.of_file ymd)) |> respond'
 let form_response ymd = `Html (Html.form (Logarion.of_file ymd)) |> respond'
 
-let print_css  = get "/style.css" @@ (fun _ -> file_response "ymd/style.css")
-let print_ymd  = get "/:title" @@ (fun req -> filepath_of_param req "title" |> ymd_response)
-let print_form = get "/()/new" @@ (fun _ -> `Html (Html.form (Ymd.blank_ymd)) |> respond')
-
-let process_form = post "/()/new" (fun req -> let ymd = ymd_of_req req in Logarion.to_file ymd; `Html (Html.of_ymd ymd) |> respond')
-let edit_form = get "/:title/edit" (fun req -> filepath_of_param req "title" |> form_response)
-let print_toc = get "/" (fun req -> `Html (Html.of_titled_files (Logarion.titled_files ())) |> respond')
-
 let _ =
   App.empty
-  |> print_ymd
-  |> print_form
-  |> edit_form
-  |> process_form
-  |> print_css
-  |> print_toc
+  |> get "/:ttl"      (fun req -> filepath_of_param req "ttl" |> ymd_response)
+  |> get "/:ttl/edit" (fun req -> filepath_of_param req "ttl" |> form_response)
+  |> get "/style.css" (fun _ -> file_response "ymd/style.css")
+  |> get "/()/new"    (fun _ -> `Html (Html.form (Ymd.blank_ymd)) |> respond')
+  |> post "/()/new"   (fun req -> let ymd = ymd_of_req req in Logarion.to_file ymd; `Html (Html.of_ymd ymd) |> respond')
+  |> get "/"          (fun req -> `Html (Html.of_titled_files (Logarion.titled_files ())) |> respond')
   |> App.run_command
